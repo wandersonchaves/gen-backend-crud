ab866fa009da58b43bb884d042f0197d9718c488
Wanderson Chaves <wandersonchaves@goapice.com>
2024-09-19 16:56:36 -0300

    feat: add cors to frontend url

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import {NestFactory} from '@nestjs/core'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.enableCors({
+    origin: 'http://localhost:4200', // Pode colocar '*' para permitir qualquer origem, mas é melhor especificar.
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  })
+
   await app.listen(3000)
 }
 bootstrap()
